50d73a77f64d0226922ae9d27e909f67e5dd1946
Dmitry Matveyev <dm.matveyev@protonmail.com>
2020-03-25 08:18:09 +0300

    Add telegram default tag for dwm

--- dwm/config.h
+++ dwm/config.h
@@ -26,10 +26,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Chromium", NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Emacs",    NULL,       NULL,       1 << 3,       0,           -1 },
+	/* class         instance    title       tags mask     isfloating   monitor */
+	{ "Firefox",     NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Chromium",    NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Emacs",       NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Telegram",    NULL,       NULL,       1 << 5,       0,           -1 },
 };
 
 /* layout(s) */
