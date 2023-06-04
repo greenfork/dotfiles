32f1b4baccf4965e0491ea44728af16c7b4f21a3
Dmitry Matveyev <public@greenfork.me>
2023-06-04 17:07:14 +0600

    Add mu script to re-initialize the index

---
+++ mu_init.sh
@@ -0,0 +1,1 @@
+mu init --maildir ~/mail --my-address=greenfork.lists@yandex.com --my-address=dev@greenfork.me --my-address=news@greenfork.me --my-address=hello@greenfork.me --my-address=public@greenfork.me
